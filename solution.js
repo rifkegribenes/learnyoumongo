9d4fc63abd86dba52cc159f5ffbaf54cef6825d8
Sarah Schneider <rifkegribenes@gmail.com>
2018-01-14 22:25:17 +0000

    exercise 4

--- solution.js
+++ solution.js
@@ -6,6 +6,10 @@ var mongo = require('mongodb').MongoClient
     const age = process.argv[2];
     db.collection('parrots').find({
       age: { $gt: +age } 
+    }, {
+      name: 1,
+      age: 1,
+      _id: 0
     }).toArray(function(err, documents) {
       if (err) throw err;
   		console.log(documents);
