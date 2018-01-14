92976d3816349a8cf85036186408e1b4b3c60f72
Sarah Schneider <rifkegribenes@gmail.com>
2018-01-14 23:06:28 +0000

    exercise 9

--- solution.js
+++ solution.js
@@ -1,14 +1,22 @@
 const url = 'mongodb://localhost:27017/learnyoumongo';
-const age = process.argv[2];
+const size = process.argv[2];
 var mongo = require('mongodb').MongoClient
   mongo.connect(url, function(err, db) {
     if (err) throw err;
-    const collection = db.collection('parrots');
-    collection.count({
-      age: { $gt: +age } 
-    }, (err, count) => {
+    const collection = db.collection('prices'); 
+    collection.aggregate([
+      { $match: { size: size } },
+      { $group: {
+        _id: 'average',
+        average: { $avg: '$price' }
+      }}
+    ]).toArray((err, results) => {
       if (err) throw err;
-      console.log(count);
+      if (!results.length) {
+          throw new Error('No results found')
+        }
+      const o = results[0];
+      console.log(Number(o.average).toFixed(2));
       db.close();
     });
 });
