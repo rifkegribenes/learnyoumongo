fd4516b8c78001abc9d188b8dfb14a28962b174d
Sarah Schneider <rifkegribenes@gmail.com>
2018-01-14 22:21:33 +0000

    exercise 3

---
+++ solution.js
@@ -0,0 +1,14 @@
+const url = 'mongodb://localhost:27017/learnyoumongo';
+var mongo = require('mongodb').MongoClient
+  mongo.connect(url, function(err, db) {
+    if (err) throw err;
+    const parrots = db.collection('parrots');
+    const age = process.argv[2];
+    db.collection('parrots').find({
+      age: { $gt: +age } 
+    }).toArray(function(err, documents) {
+      if (err) throw err;
+  		console.log(documents);
+  		db.close();
+  });
+});
