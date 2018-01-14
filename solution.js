b1c86071d17f00b7745e89d85041f42978a3b475
Sarah Schneider <rifkegribenes@gmail.com>
2018-01-14 22:37:18 +0000

    exercise 5

--- solution.js
+++ solution.js
@@ -2,17 +2,9 @@ const url = 'mongodb://localhost:27017/learnyoumongo';
 var mongo = require('mongodb').MongoClient
   mongo.connect(url, function(err, db) {
     if (err) throw err;
-    const parrots = db.collection('parrots');
-    const age = process.argv[2];
-    db.collection('parrots').find({
-      age: { $gt: +age } 
-    }, {
-      name: 1,
-      age: 1,
-      _id: 0
-    }).toArray(function(err, documents) {
-      if (err) throw err;
-  		console.log(documents);
-  		db.close();
-  });
+    const users = db.collection('users');
+    const obj = {firstName: process.argv[2], lastName: process.argv[3]};
+    users.insert(obj);
+    console.log(JSON.stringify(obj));
+  	db.close();
 });
