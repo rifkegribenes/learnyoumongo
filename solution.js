b1c6f791f0f4da2b4873f3dabaf204d2774165b8
Sarah Schneider <rifkegribenes@gmail.com>
2018-01-14 22:51:42 +0000

    exercise 7

--- solution.js
+++ solution.js
@@ -1,14 +1,11 @@
 const url = `mongodb://localhost:27017/${process.argv[2]}`;
+const id = process.argv[4];
 var mongo = require('mongodb').MongoClient
   mongo.connect(url, function(err, db) {
     if (err) throw err;
-    const users = db.collection('users');
-    users.update({
-      username: "tinatime"
-    }, {
-      $set: {
-        age: 40
-      }
+    const collection = db.collection(process.argv[3]);
+    collection.remove({
+      _id: id
     }, (err) => {
       if (err) throw err;
       db.close();
